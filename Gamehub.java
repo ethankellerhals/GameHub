40f27a8e54c20ef53de048135e32664cf8f5bef4
goinsj99 <jogo8350@colorado.edu>
2023-04-10 16:23:36 -0600

    Game and User objects

--- Gamehub.java
+++ Gamehub.java
@@ -13,5 +13,26 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.FileOutputStream; 
 public class Gamehub {
-    
+    public List<Games> gameList = new ArrayList();
+
+    public Gamehub(){
+        this.gameList = setLibrary(gameList,"gameDataset.csv");
+    }
+    public List<String> setLibrary(List<String> currList, String filesname){
+        try {
+            File file = new File(filesname);
+            Scanner scanner = new Scanner(file);
+
+            while (scanner.hasNextLine()) {
+                String line1 = scanner.nextLine();
+                currList.add(line1);
+            }
+
+            scanner.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return currList;
+    }
 }
