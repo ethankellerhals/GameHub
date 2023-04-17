1d32588b30d8e76dcc9604f4e7b22d7e3554b43f
ethankellerhals <ethan.kellerhals@colorado.edu>
2023-04-17 17:58:29 -0600

    edit

---
+++ GameHubApp/app/src/main/java/com/gamehub/myapplication/EditProfileActivity.java
@@ -0,0 +1,22 @@
+package com.gamehub.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class EditProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_profile);
+
+//        Button saveChangesBtn = findViewById(R.id.saveChangesButton);
+//        saveChangesBtn.setOnClickListener(new View.OnClickListener() {
+//           @Override
+//           public void onClick(View v) {
+//               finish();
+//           }
+//        });
+    }
+}
--- GameHubApp/app/src/main/java/com/gamehub/myapplication/ProfileActivity.java
+++ GameHubApp/app/src/main/java/com/gamehub/myapplication/ProfileActivity.java
@@ -2,6 +2,8 @@ package com.gamehub.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -9,5 +11,25 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+
+//        Button customizeBtn = findViewById(R.id.customizeButton);
+//        customizeBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                //LayoutInflater inflater = getLayoutInflater();
+//
+////                View customizeFormView = getLayoutInflater().inflate(R.layout.profile_edit_form, null);
+////                ViewGroup parent = (ViewGroup) findViewById(android.R.id.content);
+////                parent.removeAllViews();
+////                parent.addView(editFormView);
+//
+////                AlertDialog.Builder builder = new AlertDialog.Builder(ProfileActivity.this);
+////                build.setView(editFormView);
+////                builder.create().show();
+//            }
+//        });
     }
+
+
 }
