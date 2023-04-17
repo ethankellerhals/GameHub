d3e12954e4fb654e7f7f63d6fbb7a948a9dcbaf9
ethankellerhals <ethan.kellerhals@colorado.edu>
2023-04-16 20:50:05 -0600

    still wokring on projfile

--- GameHubApp/app/src/main/java/com/gamehub/myapplication/MainActivity.java
+++ GameHubApp/app/src/main/java/com/gamehub/myapplication/MainActivity.java
@@ -1,13 +1,29 @@
 package com.gamehub.myapplication;
 
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageButton profileBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // after clicking the profile button, take to profile page
+        profileBtn = (ImageButton) findViewById(R.id.profileButton);
+        profileBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentLoadNewActivity = new Intent(MainActivity.this, ProfileActivity.class);
+                MainActivity.this.startActivity(intentLoadNewActivity);
+            }
+        });
     }
 }
--- GameHubApp/app/src/main/java/com/gamehub/myapplication/ProfileActivity.java
+++ GameHubApp/app/src/main/java/com/gamehub/myapplication/ProfileActivity.java
@@ -3,11 +3,11 @@ package com.gamehub.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class ProfilePage extends AppCompatActivity {
+public class ProfileActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile_page);
+        setContentView(R.layout.activity_profile);
     }
 }
