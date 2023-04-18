6b66ba64fa74840179c9a8d7197bc39b5216fab0
goinsj99 <jogo8350@colorado.edu>
2023-04-18 14:08:33 -0600

    User and game objects updated

--- GameHubApp/app/src/main/java/com/gamehub/myapplication/MainActivity.java
+++ GameHubApp/app/src/main/java/com/gamehub/myapplication/MainActivity.java
@@ -11,8 +11,10 @@ public class MainActivity extends AppCompatActivity {
 
     ImageButton profileBtn;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
---
+++ GameHubApp/app/src/main/java/com/gamehub/myapplication/users.java
@@ -0,0 +1,68 @@
+package com.gamehub.myapplication;
+import java.util.List;
+
+public class users {
+    private Integer userID;
+    private String userName;
+    private String firstName;
+    private List<games> currentPlay;
+    private List <games> havePlayed;
+    private List <users> myFriends;
+    private String userEmail;
+    // Not done * char
+    public users(Integer userID, String userName, String firstName, String userEmail, List<games> currentPlay ,List<games> havePlayed, List<users> myFriends ){
+        this.userID = userID;
+        this.userName = userName;
+        this.firstName = firstName;
+        this.userEmail = userEmail;
+        this.currentPlay = currentPlay;
+        this.havePlayed = havePlayed;
+        this.myFriends = myFriends;
+    }
+    public void setUserID(Integer userID){
+        this.userID = userID;
+
+    }
+    public void setUserName(String userName){
+        this.userName = userName;
+
+    }
+    public void setFirstName(String firstName){
+        this.firstName = firstName;
+
+    }
+    public void setUserEmail(String userEmail){
+        this.userEmail = userEmail;
+
+    }
+    public void setCurrentPlay(List<games> currentPlay){
+        this.currentPlay = currentPlay;
+    }
+    public void setHavePlayed(List<games> havePlayed){
+        this.havePlayed = havePlayed;
+    }
+    public void setMyFriends(List<users> myFriends){
+        this.myFriends = myFriends;
+    }
+    public List<games> getCurrentPlay(){
+        return this.currentPlay;
+    }
+    public List<games> getHavePlayed(){
+        return this.havePlayed;
+    }
+    public List<users> getMyFriends(){
+        return this.myFriends;
+    }
+    public Integer getUserID(){
+        return this.userID;
+    }
+    public String getuserName(){
+        return this.userName;
+    }
+    public String getfirstName(){
+        return this.firstName;
+    }
+    public String userEmail(){
+        return this.userEmail;
+    }
+}
