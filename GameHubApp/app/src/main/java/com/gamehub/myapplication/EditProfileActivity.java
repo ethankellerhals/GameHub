28d189555e1b4bc78bf9020e6b79afefcf114888
ethankellerhals <ethan.kellerhals@colorado.edu>
2023-04-18 17:15:19 -0600

    update

--- GameHubApp/app/src/main/java/com/gamehub/myapplication/EditProfileActivity.java
+++ GameHubApp/app/src/main/java/com/gamehub/myapplication/EditProfileActivity.java
@@ -1,16 +1,25 @@
 package com.gamehub.myapplication;
-
+import java.io.IOException;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.content.Intent;
+import android.media.Image;
+import android.provider.MediaStore.Images;
+import android.net.Uri;
+import android.graphics.Bitmap;
+import android.widget.ImageView;
 
 public class EditProfileActivity extends AppCompatActivity {
 
+    private static final int PICK_IMAGE_REQUEST = 1;
+    private ImageView profilePictureImageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
 
+        //profilePictureImageView = findViewById(R.id.profilePicture);
 //        Button saveChangesBtn = findViewById(R.id.saveChangesButton);
 //        saveChangesBtn.setOnClickListener(new View.OnClickListener() {
 //           @Override
@@ -18,5 +27,27 @@ public class EditProfileActivity extends AppCompatActivity {
 //               finish();
 //           }
 //        });
+    }
+
+    public void selectNewProfilePicture(View view) {
+        Intent intent = new Intent(Intent.ACTION_PICK, Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, PICK_IMAGE_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            Uri imageUri = data.getData();
+
+            try {
+                Bitmap bitmap = Images.Media.getBitmap(getContentResolver(), imageUri);
+                ImageView profilePic = findViewById(R.id.profilePicture);
+                profilePic.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
--- GameHubApp/app/src/main/java/com/gamehub/myapplication/ProfileActivity.java
+++ GameHubApp/app/src/main/java/com/gamehub/myapplication/ProfileActivity.java
@@ -22,26 +22,18 @@ public class ProfileActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intentLoadNewActivity = new Intent(ProfileActivity.this, EditProfileActivity.class);
                 ProfileActivity.this.startActivity(intentLoadNewActivity);
+
+//                LayoutInflater inflater = getLayoutInflater();
+//
+//                View customizeFormView = getLayoutInflater().inflate(R.layout.activity_edit_profile, null);
+//                ViewGroup parent = (ViewGroup) findViewById(android.R.id.content);
+//                parent.removeAllViews();
+//                parent.addView(editFormView);
+//
+//                AlertDialog.Builder builder = new AlertDialog.Builder(ProfileActivity.this);
+//                build.setView(editFormView);
+//                builder.create().show();
             }
         });
-
-//        Button customizeBtn = findViewById(R.id.customizeButton);
-//        customizeBtn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                //LayoutInflater inflater = getLayoutInflater();
-//
-////                View customizeFormView = getLayoutInflater().inflate(R.layout.profile_edit_form, null);
-////                ViewGroup parent = (ViewGroup) findViewById(android.R.id.content);
-////                parent.removeAllViews();
-////                parent.addView(editFormView);
-//
-////                AlertDialog.Builder builder = new AlertDialog.Builder(ProfileActivity.this);
-////                build.setView(editFormView);
-////                builder.create().show();
-//            }
-//        });
     }
-
-
 }
