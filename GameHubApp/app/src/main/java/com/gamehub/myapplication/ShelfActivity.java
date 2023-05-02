9d91fe6d8f48ecca53ba6f97ada707deb373a0a4
Adrian Ornelas <Ador2698@colorado.edu>
2023-05-02 17:50:08 -0600

    added list to shelfs

--- GameHubApp/app/src/main/java/com/gamehub/myapplication/ShelfActivity.java
+++ GameHubApp/app/src/main/java/com/gamehub/myapplication/ShelfActivity.java
@@ -13,6 +13,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.PopupWindow;
 import android.widget.TextView;
 
@@ -330,6 +331,151 @@ public class ShelfActivity extends AppCompatActivity {
                 });
             }
         });
+
+        // currently playing button
+        Button curr_p_b_s = findViewById(R.id.CurrentlyPlayingButton);
+        curr_p_b_s.setOnClickListener(new View.OnClickListener() {
+            // declare variables
+            private ImageView game1view;
+            private TextView game1txt;
+            private ImageView game2view;
+            private TextView game2txt;
+            private ImageView game3view;
+            private TextView game3txt;
+            private ImageView game4view;
+            private TextView game4txt;
+            private ImageView game5view;
+            private TextView game5txt;
+            private ImageView game6view;
+            private TextView game6txt;
+
+            @Override
+            public void onClick(View v) {
+                // Create a PopupWindow
+                LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                View popupView = inflater.inflate(R.layout.five_game_view, null);
+                // create the popup window
+                int width = LinearLayout.LayoutParams.WRAP_CONTENT;
+                int height = LinearLayout.LayoutParams.WRAP_CONTENT;
+                boolean focusable = true; // let popup window handle touch events
+                PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
+
+                // initalize the views
+                game1view = popupView.findViewById(R.id.game1);
+                    game1txt = popupView.findViewById(R.id.game1text);
+                game2view = popupView.findViewById(R.id.game2);
+                    game2txt = popupView.findViewById(R.id.game2text);
+                game3view = popupView.findViewById(R.id.game3);
+                    game3txt = popupView.findViewById(R.id.game3text);
+                game4view = popupView.findViewById(R.id.game4);
+                    game4txt = popupView.findViewById(R.id.game4text);
+                game5view = popupView.findViewById(R.id.game5);
+                    game5txt = popupView.findViewById(R.id.game5text);
+                game6view = popupView.findViewById(R.id.game6);
+                    game6txt = popupView.findViewById(R.id.game6text);
+
+                // Show the PopupWindow
+                popupWindow.showAtLocation(topPlayBo, Gravity.CENTER, 0, 0);
+
+                // Set the image and text of the views
+                game1view.setImageResource(R.drawable.fortnite);
+                    game1txt.setText("Hours: 113");
+                game2view.setImageResource(R.drawable.bo2);
+                    game2txt.setText("Hours: 145");
+                game3view.setImageResource(R.drawable.minecraft);
+                    game3txt.setText("Hours: 99");
+                game4view.setImageResource(R.drawable.gow);
+                    game4txt.setText("Hours: 65");
+                game5view.setImageResource(R.drawable.roblox);
+                    game5txt.setText("Hours: 59");
+                game6view.setImageResource(R.drawable.assassin_mir);
+                    game6txt.setText("Hours: 37");
+
+
+                // get a reference to the Button and set its OnClickListener
+                Button closeButton = popupView.findViewById(R.id.back_self_page);
+                closeButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        // close the popup window
+                        popupWindow.dismiss();
+                    }
+                });
+            }
+        });
+
+        // friends playing button
+        Button fri_p_b_s = findViewById(R.id.TopWithFriendsButton);
+        fri_p_b_s.setOnClickListener(new View.OnClickListener() {
+            // declare variables
+            private ImageView game1view;
+            private TextView game1txt;
+            private ImageView game2view;
+            private TextView game2txt;
+            private ImageView game3view;
+            private TextView game3txt;
+            private ImageView game4view;
+            private TextView game4txt;
+            private ImageView game5view;
+            private TextView game5txt;
+            private ImageView game6view;
+            private TextView game6txt;
+
+            @Override
+            public void onClick(View v) {
+                // Create a PopupWindow
+                LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                View popupView = inflater.inflate(R.layout.five_game_view, null);
+                // create the popup window
+                int width = LinearLayout.LayoutParams.WRAP_CONTENT;
+                int height = LinearLayout.LayoutParams.WRAP_CONTENT;
+                boolean focusable = true; // let popup window handle touch events
+                PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
+
+                // initalize the views
+                game1view = popupView.findViewById(R.id.game1);
+                    game1txt = popupView.findViewById(R.id.game1text);
+                game2view = popupView.findViewById(R.id.game2);
+                    game2txt = popupView.findViewById(R.id.game2text);
+                game3view = popupView.findViewById(R.id.game3);
+                    game3txt = popupView.findViewById(R.id.game3text);
+                game4view = popupView.findViewById(R.id.game4);
+                    game4txt = popupView.findViewById(R.id.game4text);
+                game5view = popupView.findViewById(R.id.game5);
+                    game5txt = popupView.findViewById(R.id.game5text);
+                game6view = popupView.findViewById(R.id.game6);
+                    game6txt = popupView.findViewById(R.id.game6text);
+
+                // Show the PopupWindow
+                popupWindow.showAtLocation(topPlayBo, Gravity.CENTER, 0, 0);
+
+                // Set the image and text of the views
+                game1view.setImageResource(R.drawable.rainbowsix);
+                    game1txt.setText("Friends: 13");
+                game2view.setImageResource(R.drawable.assassin_mir);
+                    game2txt.setText("Friends: 11");
+                game3view.setImageResource(R.drawable.csgo);
+                    game3txt.setText("Friends: 10");
+                game4view.setImageResource(R.drawable.xdefi);
+                    game4txt.setText("Friends: 10");
+                game5view.setImageResource(R.drawable.roblox);
+                    game5txt.setText("Friends: 9");
+                game6view.setImageResource(R.drawable.batfei);
+                    game6txt.setText("Friends: 5");
+
+
+                // get a reference to the Button and set its OnClickListener
+                Button closeButton = popupView.findViewById(R.id.back_self_page);
+                closeButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        // close the popup window
+                        popupWindow.dismiss();
+                    }
+                });
+            }
+        });
+
 
     }
 }
