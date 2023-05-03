3d4477e235c2dd8d4119089b1e292c490bbe0386
ethankellerhals <59932400+ethankellerhals@users.noreply.github.com>
2023-05-03 06:01:47 +0000

    aaaa
    aaaaa

--- GameHubApp/app/src/main/java/com/gamehub/myapplication/GenreActivity.java
+++ GameHubApp/app/src/main/java/com/gamehub/myapplication/GenreActivity.java
@@ -3,9 +3,18 @@ package com.gamehub.myapplication;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.SearchView;
+
+import java.sql.Array;
+import java.util.List;
+import java.util.ArrayList;
+//import
+import android.util.Log;
 
 public class GenreActivity extends AppCompatActivity {
 
@@ -15,6 +24,10 @@ public class GenreActivity extends AppCompatActivity {
     Button shelfBtn;
     Button gamesBtn;
     Button friendBtn;
+    SearchView searchBar;
+    private SearchController searchController;
+
+
 
 
     @Override
@@ -22,6 +35,10 @@ public class GenreActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_genre);
 
+        List<games> gamesList = new ArrayList<>();
+        List<users> usersList = new ArrayList<>();
+        searchController = new SearchController(gamesList, usersList);
+        RecyclerView recyclerView = findViewById(R.id.search_results_recyclerview);
         // after clicking the profile button, take to profile page
         profileBtn = (ImageButton) findViewById(R.id.profileButton);
         // shelf button, take to shelf page
@@ -34,6 +51,40 @@ public class GenreActivity extends AppCompatActivity {
         logBtn = (ImageButton) findViewById(R.id.addButton);
         // home button, take to home page
         homeBtn = (ImageButton) findViewById(R.id.homeButton);
+
+        searchBar = findViewById(R.id.searchBarSearchView);
+        List<SearchResult> dummyResults = new ArrayList<>();
+        dummyResults.add(new SearchResult("Ass", ResultType.GAME));
+        dummyResults.add(new SearchResult("money", ResultType.USER));
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        SearchResultsAdapter adapter = new SearchResultsAdapter(dummyResults);
+
+        //SearchResultsAdapter adapter = new SearchResultsAdapter(new ArrayList<>());
+        recyclerView.setAdapter(adapter);
+        searchBar.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                List<SearchResult> results = new ArrayList<>();
+                results.addAll(searchController.searchGames(query));
+                results.addAll(searchController.searchUsers(query));
+                adapter.setData(results);
+                Log.d("11", "1");
+//                SearchResultsAdapter adapter = new SearchResultsAdapter(results);
+//                recyclerView.setAdapter(adapter);
+                return true;
+            }
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                List<SearchResult> results = new ArrayList<>();
+                results.addAll(searchController.searchGames(newText));
+                results.addAll(searchController.searchUsers(newText));
+//                SearchResultsAdapter adapter = new SearchResultsAdapter(results);
+//                recyclerView.setAdapter(adapter);
+                adapter.setData(results);
+                Log.d("1", "111");
+                return true;
+            }
+        });
 
         logBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -81,5 +132,8 @@ public class GenreActivity extends AppCompatActivity {
             }
         });
 
+
+
     }
+
 }
---
+++ GameHubApp/app/src/main/java/com/gamehub/myapplication/SearchController.java
@@ -0,0 +1,40 @@
+package com.gamehub.myapplication;
+import java.util.List;
+import java.util.ArrayList;
+public class SearchController {
+    //private SearchInterface searchInterface;
+    private List<games> gamesList;
+    private List<users> usersList;
+    private SearchInterface gameTitleSearch;
+    private SearchInterface usernameSearch;
+//    public void setSearchInterface(SearchInterface searchInterface) {
+//        this.searchInterface = searchInterface;
+//    }
+
+    public SearchController(List<games> gamesList, List<users> usersList) {
+        this.gamesList = gamesList;
+        this.usersList = usersList;
+        this.gameTitleSearch = new GameTitleSearch(gamesList);
+        this.usernameSearch = new UsernameSearch(usersList);
+    }
+
+    public List<SearchResult> searchGames(String searchTerm) {
+        return gameTitleSearch.search(searchTerm);
+    }
+    public List<SearchResult> searchUsers(String searchTerm) {
+        return usernameSearch.search(searchTerm);
+    }
+//    public List<SearchResult> performSearch(String searchTerm) {
+//        if (searchInterface == null) {
+//            throw new IllegalStateException("Search strategy not selected");
+//        }
+//        return searchInterface.search(searchTerm);
+//    }
+//    public List<SearchResult> performSearch(SearchInterface searchInterface, String searchTerm) {
+//        List<SearchResult> searchResults = new ArrayList<>();
+//        if (seachInterface instanceof GameTitleSearch) {
+//            GameTitleSearch gameTitleSearch = (GameTitleSearch) searchInterface;
+//            searchResults.addAll(gameTitleSearch.searchByTitle(searchTerm));
+//        }
+//    }
+}
